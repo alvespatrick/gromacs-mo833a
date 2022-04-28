a132e700538f25ead9e32f9d283853532b841749
Patrick Alves <p265061@dac.unicamp.br>
2022-04-28 20:11:54 -0300

    Update

--- experiments/ativ-3-exp-1/simulation_release.sh
+++ experiments/ativ-3-exp-1/simulation_release.sh
@@ -5,7 +5,7 @@ echo "15" | ../../build/bin/gmx pdb2gmx -f 6LVN.pdb -o 6LVN_processed.gro -water
 echo "13" | ../../build/bin/gmx genion -s ions.tpr -o 6LVN_solv_ions.gro -p topol.top -pname NA -nname CL -neutral
 ../../build/bin/gmx grompp -f ions.mdp -c 6LVN_solv_ions.gro -p topol.top -o em.tpr
 
-for i in seq 9; do
+for i in $(seq 9; do
 
 	../../build/bin/gmx mdrun -v -deffnm em >> release.out
 done
